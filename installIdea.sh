755a58ee9fedeb49275f4beb56d34d9e135253fe
Artyom Emelyanenko <tema.emelyan@gmail.com>
2018-05-25 15:09:17 +0000

    lol

--- installIdea.sh
+++ installIdea.sh
@@ -1,4 +1,4 @@
 -#!/bin/bash
-cp ./forCreateDevEnvScript/.m2 ~ -r
-cp ./forCreateDevEnvScript/.IntelliJIdea2018.2/ ~ -r
+cp ./lol/.m2 ~ -r
+cp ./lol/.IntelliJIdea2018.2/ ~ -r
 sudo snap install intellij-idea-ultimate --classic --edge
